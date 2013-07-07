45a43d40cfb943f385bdff467d278eca3c718a9c
INABA Hitoshi <ina@cpan.org>
2013-07-07 14:29:22 +0000

    Import of INA/Char-Cyrillic-0.91 from CPAN.
    gitpan-cpan-distribution: Char-Cyrillic
    gitpan-cpan-version:      0.91
    gitpan-cpan-path:         INA/Char/Cyrillic/Char-Cyrillic-0.91.tar.gz
    gitpan-cpan-author:       INA
    gitpan-cpan-maturity:     released

--- Cyrillic.pm
+++ Cyrillic.pm
@@ -29,7 +29,7 @@ BEGIN {
 # (and so on)
 
 BEGIN { eval q{ use vars qw($VERSION) } }
-$VERSION = sprintf '%d.%02d', q$Revision: 0.90 $ =~ /(\d+)/oxmsg;
+$VERSION = sprintf '%d.%02d', q$Revision: 0.91 $ =~ /(\d+)/oxmsg;
 
 BEGIN { CORE::require Ecyrillic; }
 
@@ -489,7 +489,7 @@ sub Cyrillic::escape_script {
         # in Chapter 5: Pattern Matching
         # of ISBN 978-0-596-00492-7 Programming Perl 4th Edition.
 
-        $e_script .= sprintf("use Ecyrillic %s;\n", $Cyrillic::VERSION); # require run-time routines version
+        $e_script .= sprintf("use Ecyrillic %s.0;\n", $Cyrillic::VERSION); # require run-time routines version
 
         # use Cyrillic version qw(ord reverse getc);
         $function_ord     = 'ord';
@@ -5411,12 +5411,6 @@ Cyrillic.pm removes 'bytes::' at head of function name.
   bytes::substr    substr    Byte
   ---------------------------------------
 
-=head1 Escaping Built-in Standard Module (Ecyrillic.pm provides)
-
-Ecyrillic.pm does "BEGIN { unshift @INC, '/Perl/site/lib/Cyrillic' }" at head.
-Store the standard module modified for Cyrillic software in this directory to
-override built-in standard modules.
-
 =head1 Escaping Standard Module Content (You do)
 
 You need copy built-in standard module to /Perl/site/lib/Cyrillic and change
@@ -6052,3 +6046,4 @@ I am thankful to all persons.
 
 =cut
 
+# Did you fun enough? ^o^)
